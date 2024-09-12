ae69d13cb0aa653a27d904da8badb8bc8825641f
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-09-12 11:57:08 +0400

    Misc updates

--- orders/src/main/java/com/company/orders/exception/RestDataStoreAccessExceptionHandler.java
+++ orders/src/main/java/com/company/orders/exception/RestDataStoreAccessExceptionHandler.java
@@ -3,12 +3,16 @@ package com.company.orders.exception;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.exception.AbstractUiExceptionHandler;
 import io.jmix.restds.exception.RestDataStoreAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestDataStoreAccessExceptionHandler extends AbstractUiExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(RestDataStoreAccessExceptionHandler.class);
 
     private final Notifications notifications;
 
@@ -19,6 +23,8 @@ public class RestDataStoreAccessExceptionHandler extends AbstractUiExceptionHand
 
     @Override
     protected void doHandle(@NonNull String className, @NonNull String message, @Nullable Throwable throwable) {
+        log.warn(message);
+
         if (throwable instanceof RestDataStoreAccessException exception) {
             notifications.create("Connection error",
                             "'" + exception.getDataStoreName() + "' service is unavailable")
